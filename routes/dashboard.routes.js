2990782403ee1cdce365ae05b00af4bd4b8d92aa
Sushanpoojari <91779688+Sushanpoojari@users.noreply.github.com>
2025-07-05 13:12:12 +0530

    another minor change

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -1,8 +1,7 @@
 import express from "express";
-import {authenticateAccessToken} from '../middleware/authMiddleware'
 import {  fetchDashboardData } from "../controllers/dashboard.controller";
 const dashboardRouter= express.Router()
 
-dashboardRouter.post("/dashboard",authenticateAccessToken,fetchDashboardData)
+dashboardRouter.post("/dashboard",fetchDashboardData)
 
 export default dashboardRouter;
